396e8ac4228fc0d592792f9df10e842e59583f8b
ArtimNayas <sayan.mitra@shopup.org>
2022-06-25 16:08:27 +0530

    Basic Setup and Connect Firebase - Database and Authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useRef} from "react";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+import 'firebase/compat/auth'
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCbhbWct2ZvZfrbsku6w3SuEO4ZzegRWhE",
+  authDomain: "react-chat-app-adb4e.firebaseapp.com",
+  projectId: "react-chat-app-adb4e",
+  databaseURL: "https:/react-chat-app-adb4e.firebaseio.com",
+  storageBucket: "react-chat-app-adb4e.appspot.com",
+  messagingSenderId: "184170737383",
+  appId: "1:184170737383:web:8fa3594d0edc3e03a5bc42",
+  measurementId: "G-BZM5FY4XGT"
+})
 
 function App() {
+
+  const[color,setColor] = useState("red")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Cuvette">
+      <h1>My favourite colour is {color}</h1>
+
+      <button type = "button"onClick = {() => setColor("blue")}>BLUE</button>
+      <button type = "button"onClick = {() => setColor("red")}>RED</button>
     </div>
   );
 }
