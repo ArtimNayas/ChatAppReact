8b2617a03c9f86e7deb2129ff1d719ba5c45a596
ArtimNayas <sayan.mitra@shopup.org>
2022-06-26 01:19:52 +0530

    Complete Frontend without CSS

--- src/App.js
+++ src/App.js
@@ -4,81 +4,128 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore'; 
 import {useAuthState} from 'react-firebase-hooks/auth';
-
+import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCbhbWct2ZvZfrbsku6w3SuEO4ZzegRWhE",
-  authDomain: "react-chat-app-adb4e.firebaseapp.com",
-  projectId: "react-chat-app-adb4e",
-  databaseURL: "https:/react-chat-app-adb4e.firebaseio.com",
-  storageBucket: "react-chat-app-adb4e.appspot.com",
-  messagingSenderId: "184170737383",
-  appId: "1:184170737383:web:8fa3594d0edc3e03a5bc42",
-  measurementId: "G-BZM5FY4XGT"
+  apiKey: "AIzaSyANSMGjcEWQrvaEFgwd0g9qKTJGDFQhNfo",
+  authDomain: "react-chat-app-fbd52.firebaseapp.com",
+  projectId: "react-chat-app-fbd52",
+  databaseURL: "https://react-chat-app-fbd52.firebaseio.com",
+  storageBucket: "react-chat-app-fbd52.appspot.com",
+  messagingSenderId: "878948549024",
+  appId: "1:878948549024:web:d505a430d8397d6dee64c5",
+  measurementId: "G-ZM1C9Y8HB9"
 })
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-
+ 
 function App() {
-
+  
   const[user] = useAuthState(auth);
-
   return (
-    <div className='App'>
+    <div className = "App">
+ 
       <header>
-        <h1>This is a Chat Service</h1>
+        <h1>THIS A CHAT SERVER </h1>
         <SignOut />
       </header>
-
+ 
       <section>
-        {user ? <Chatroom /> : <SignIn />}
+        {user ? <ChatRoom /> : <SignIn />} 
       </section>
-
+ 
     </div>
-
+ 
   );
+ 
 }
-
+ 
 function SignIn(){
-
-  const signInWithGoogle = () =>{
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return(
+ 
+    const signInWithGoogle = () =>{
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+ 
+    return (
     <>
-      <button className="sign-in" onClick = {signInWithGoogle}>Sign In With Google</button>
+    <button className = "sign-in" onClick = {signInWithGoogle}>Sign In with Google </button>
     </>
-  )
-
+    )
 }
-
-function SignOut(){
-
-  return auth.currentUser && (
-    <button className='sign-out' onClick={() => auth.signOut()}>Sign Out</button>
-  )
-
+ 
+function SignOut() {
+ 
+    return auth.currentUser && (
+      <button className = "sign-out" onClick = {() => auth.signOut()}>Sign Out</button>
+    )
 }
-
-function Chatroom(){
-
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, {idField: 'id'})
-
-  return (
-    <>
-      <main>
-        {messages && messages.map(msg => <ChatMessage key = {msg.id} message = {msg}/>)}
-      </main>
-    </>
-  )
-
+ 
+function ChatRoom(){
+ 
+    //Display the messages
+    const messagesRef = firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(25);
+    const [messages] = useCollectionData(query,{idField: 'id'});
+ 
+    const [formValue, setFormValue] = useState('');
+ 
+    const {uid,photoURL} = auth.currentUser;
+ 
+    const sendMessage = async(e) => {
+ 
+        e.preventDefault();
+ 
+        await messagesRef.add({
+ 
+          text : formValue,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+          uid,
+          photoURL
+        })
+        setFormValue('');
+ 
+    }
+    return (
+ 
+      <>
+        <main>
+ 
+            {messages && messages.map(msg => <ChatMessage key = {msg.id} message = {msg}/>)}
+        
+        </main>
+ 
+        <form onSubmit = {sendMessage} >
+ 
+          <input value = {formValue} onChange={(e) => setFormValue(e.target.value)} >
+          </input>
+          <button type = "submit" disabled = {!formValue} > Send Message</button>
+        </form>
+      </>
+ 
+    )
+ 
 }
-
+ 
+function ChatMessage(props){
+ 
+    const {text,uid,photoURL} = props.message;
+ 
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+ 
+    return (
+ 
+      <>  
+        <div className = {`message ${messageClass}`}>
+        <img src = {photoURL || 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'} />
+        </div>
+        <p>{text}</p>
+      </>
+ 
+    )
+ 
+}
+ 
 export default App;
