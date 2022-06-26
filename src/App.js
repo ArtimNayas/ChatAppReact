bc62fefee4f063314bdae458413e45ff399ff5f0
ArtimNayas <sayan.mitra@shopup.org>
2022-06-26 20:03:30 +0530

    Format code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{useState,useRef} from "react";
+import React,{useState} from "react";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore'; 
